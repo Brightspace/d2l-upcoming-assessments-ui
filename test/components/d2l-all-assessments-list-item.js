e5931fd32c6f045b890d7d2c49c19312987cf72b
Chris Kraljevic <Chris.Kraljevic@D2L.com>
2018-05-01 08:20:10 -0500

    Updated status indicator logic and tests

--- test/components/d2l-all-assessments-list-item.js
+++ test/components/d2l-all-assessments-list-item.js
@@ -45,83 +45,93 @@ describe('<d2l-all-assessments-list-item>', function() {
 		Polymer.dom.flush();
 	}
 
-	[true, false].forEach(function(isCompleted) {
-		var completeString = ' is ' + (isCompleted ? '' : 'not') + ' completed';
+	function addPermutations(permutations, name) {
+		var newPermutations = [];
+		permutations.forEach(function(permutation) {
+			var perm1 = Object.assign({}, permutation);
+			perm1[name] = true;
+			newPermutations.push(perm1);
 
-		[true, false].forEach(function(isDueToday) {
-			var dueTodayString = completeString + ' and is ' + (isDueToday ? '' : 'not') + ' due today';
+			var perm2 = Object.assign({}, permutation);
+			perm2[name] = false;
+			newPermutations.push(perm2);
+		});
+		return newPermutations;
+	}
 
-			[true, false].forEach(function(isOverdue) {
-				var overdueString = dueTodayString + ' and is ' + (isOverdue ? '' : 'not') + ' overdue';
+	describe('_updateActivityStatus', function() {
+		var permutations = addPermutations([{}], 'isCompleted');
+		permutations = addPermutations(permutations, 'isDueToday');
+		permutations = addPermutations(permutations, 'isOverdue');
+		permutations = addPermutations(permutations, 'isEnded');
+		permutations = addPermutations(permutations, 'isExempt');
 
-				[true, false].forEach(function(isEnded) {
+		permutations.forEach(function(permutation) {
+			var {isCompleted, isDueToday, isOverdue, isEnded, isExempt} = permutation;
+			var testName = `when activity is ${isCompleted ? '' : 'not'} completed`
+				+ ` and is ${isDueToday ? '' : 'not'} due today`
+				+ ` and is ${isOverdue ? '' : 'not'} overdue`
+				+ ` and is ${isEnded ? '' : 'not'} ended`
+				+ ` and is ${isExempt ? '' : 'not'} exempt`;
 
-					var endedString = overdueString + ' and is ' + (isEnded ? '' : 'not') + ' ended';
+			describe('when activity' + testName, function() {
+				beforeEach(function() {
+					setAssessmentItem(isCompleted, isDueToday, isOverdue, isEnded, isExempt);
+				});
 
-					[true, false].forEach(function(isExempt) {
-						var testName = endedString + ' and is ' + (isExempt ? '' : 'not') + ' exempt';
+				it('should show Complete activity badge correctly', function() {
+					var completionAndExempt = element.querySelectorAll('d2l-status-indicator[state="success"]');
 
-						describe('when activity' + testName, function() {
-							beforeEach(function() {
-								setAssessmentItem(isCompleted, isDueToday, isOverdue, isEnded, isExempt);
-							});
-
-							it('should show Complete activity badge correctly', function() {
-								var completionAndExempt = element.querySelectorAll('d2l-status-indicator[state="success"]');
-
-								var completionInfo = Array.prototype.slice.call(completionAndExempt)
-									.find(function(statusIndicator) {
-										return statusIndicator.firstElementChild.textContent.includes('Complete');
-									});
-								expect(completionInfo).to.exist;
-
-								isCompleted && !isExempt
-									? expect(completionInfo.getAttribute('hidden')).to.be.null
-									: expect(completionInfo.getAttribute('hidden')).to.not.be.null;
-							});
-
-							it('should show Due Today badge correctly', function() {
-								var dueTodayInfo = element.$$('d2l-status-indicator[state="default"]');
-								expect(dueTodayInfo).to.exist;
-
-								!isCompleted && isDueToday && !isOverdue && !isExempt
-									? expect(dueTodayInfo.getAttribute('hidden')).to.be.null
-									: expect(dueTodayInfo.getAttribute('hidden')).to.not.be.null;
-							});
-
-							it('should show Overdue badge correctly', function() {
-								var overdueInfo = element.$$('d2l-status-indicator[state="alert"]');
-								expect(overdueInfo).to.exist;
-
-								!isCompleted && isOverdue && !isEnded && !isExempt
-									? expect(overdueInfo.getAttribute('hidden')).to.be.null
-									: expect(overdueInfo.getAttribute('hidden')).to.not.be.null;
-							});
-
-							it('should show Closed badge correctly', function() {
-								var endedInfo = element.$$('d2l-status-indicator[state="null"]');
-								expect(endedInfo).to.exist;
-
-								!isCompleted && isEnded && !isExempt
-									? expect(endedInfo.getAttribute('hidden')).to.be.null
-									: expect(endedInfo.getAttribute('hidden')).to.not.be.null;
-							});
-
-							it('should show Exempted badge correctly', function() {
-								var completionAndExempt = element.querySelectorAll('d2l-status-indicator[state="success"]');
-
-								var exemptInfo = Array.prototype.slice.call(completionAndExempt)
-									.find(function(statusIndicator) {
-										return statusIndicator.firstElementChild.textContent.includes('Exempted');
-									});
-								expect(exemptInfo).to.exist;
-
-								isExempt
-									? expect(exemptInfo.getAttribute('hidden')).to.be.null
-									: expect(exemptInfo.getAttribute('hidden')).to.not.be.null;
-							});
+					var completionInfo = Array.prototype.slice.call(completionAndExempt)
+						.find(function(statusIndicator) {
+							return statusIndicator.firstElementChild.textContent.includes('Complete');
 						});
-					});
+					expect(completionInfo).to.exist;
+
+					isCompleted
+						? expect(completionInfo.getAttribute('hidden')).to.be.null
+						: expect(completionInfo.getAttribute('hidden')).to.not.be.null;
+				});
+
+				it('should show Due Today badge correctly', function() {
+					var dueTodayInfo = element.$$('d2l-status-indicator[state="default"]');
+					expect(dueTodayInfo).to.exist;
+
+					!isCompleted && isDueToday && !isOverdue && !isExempt
+						? expect(dueTodayInfo.getAttribute('hidden')).to.be.null
+						: expect(dueTodayInfo.getAttribute('hidden')).to.not.be.null;
+				});
+
+				it('should show Overdue badge correctly', function() {
+					var overdueInfo = element.$$('d2l-status-indicator[state="alert"]');
+					expect(overdueInfo).to.exist;
+
+					!isCompleted && isOverdue && !isEnded && !isExempt
+						? expect(overdueInfo.getAttribute('hidden')).to.be.null
+						: expect(overdueInfo.getAttribute('hidden')).to.not.be.null;
+				});
+
+				it('should show Closed badge correctly', function() {
+					var endedInfo = element.$$('d2l-status-indicator[state="null"]');
+					expect(endedInfo).to.exist;
+
+					!isCompleted && isEnded
+						? expect(endedInfo.getAttribute('hidden')).to.be.null
+						: expect(endedInfo.getAttribute('hidden')).to.not.be.null;
+				});
+
+				it('should show Exempted badge correctly', function() {
+					var completionAndExempt = element.querySelectorAll('d2l-status-indicator[state="success"]');
+
+					var exemptInfo = Array.prototype.slice.call(completionAndExempt)
+						.find(function(statusIndicator) {
+							return statusIndicator.firstElementChild.textContent.includes('Exempted');
+						});
+					expect(exemptInfo).to.exist;
+
+					!isCompleted && !isEnded && isExempt
+						? expect(exemptInfo.getAttribute('hidden')).to.be.null
+						: expect(exemptInfo.getAttribute('hidden')).to.not.be.null;
 				});
 			});
 		});
